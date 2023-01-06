b1b38a735190832af0f54e499279762c67a2927e
Peter Brown <phbrown@acm.org>
2023-01-06 11:14:15 -0500

    Clamping, rounding, Unicode characters

--- CtoF.py
+++ CtoF.py
@@ -1,5 +1,7 @@
 # Program to convert Celsius to Fahrenheit temperatures.
 
+import math
+
 def main(args: list[str]) -> int:
     # Read a temperature in Celsius
     degC: float = float(input('Please enter a temperature in Celsius: '))
@@ -8,7 +10,13 @@ def main(args: list[str]) -> int:
     degF: float = (degC * 9/5) + 32
 
     # Output the result
-    print(degC, 'degrees Celsius =', degF, 'degrees Fahrenheit')
+    print(str(degC) +'\u00B0 C =', str(degF) + '\u00b0 F')
+
+    # Rounding
+    print('Nearest \u00b0F:', round(degF))
+    print('Ceiling:', math.ceil(degF))
+    print('Floor:', math.floor(degF))
+
     return 0 # Conventional return value for completing successfully
 
 if __name__ == '__main__':
--- chaos.py
+++ chaos.py
@@ -1,10 +1,18 @@
 # File: chaos.py
 # A simple program illustrating chaotic (mathematical) behavior.
+import math
 
 def main(args: list[str]) -> int:
     print('This program illustrates a chaotic function.')
     # Accumulator variable
     x: float = float(input('Enter a number between 0 and 1: '))
+
+    EPSILON: float = 0.000001 # Really small value
+
+    # Clamp x to the range [0, 1], in case the user didn't
+    x = min(x, 1 - EPSILON) # Handles x too big
+    x = max(x, 0 + EPSILON) # Handles x too small
+
     # Loop
     for i in range(10): # type: int
         # Each time through the loop, the accumulator variable
