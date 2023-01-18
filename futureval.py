6bccd4393484f3985ac3b997982777a52381ab80
Peter Brown <phbrown@acm.org>
2023-01-18 14:36:06 -0500

    Validation with if

--- futureval.py
+++ futureval.py
@@ -5,10 +5,27 @@ def main(args: list[str]) -> int:
     # Initial amount invested (principal)
     # This is the accumulator variable
     P: float = float(input('Please input the amount to invest: $'))
+    # Attempt to validate P
+    if not (P > 0): # Wall Street, we have a problem
+        print('The amount to invest must be positive.  Please try again.')
+        return 1 # No sensible way to recover
+    # No maximum investmant (practically speaking)
+
     # Interest rate *per compounding period*
     rate: float = float(input('Please input the interest rate, in percent: '))
+    if not (rate > 0):
+        print('The rate must be greater than 0.  Please try again.')
+        return 1 # No sensible way to recover
+    # No maximum interest rate (practically speaking)
+
     # Number of periods to invest for
     periods: int = int(input('Please enter the number of periods for which to invest: '))
+    if periods < 1:
+        print('The number of periods must be greater than 0.  Please try again.')
+        return 1
+    # No maximum number of periods (not even theoretically; 
+    # as the period length goes to 0, this converges to continuous compounding)
+
 
     # Echo back the initial conditions
     print('Investing $' + str(round(P, 2)), 'at', str(rate) + '% for', periods, 'periods.')
--- idmaker.py
+++ idmaker.py
@@ -1,33 +1,52 @@
 # Program to do nothing, correctly.
 
+def clean(name: str) -> str:
+    cleanname: str = ''
+    for c in name:
+        if (c.isascii() and c.isalpha()) or (c == '-'):
+            cleanname = cleanname + c
+    return cleanname
+
+def read_names(fname: str) -> list[list[str]]:
+    namelist: list[list[str]] = []
+    with open(fname,'r') as f:
+        for line in f.readlines()[1:]:
+            namelist.append(line.split(','))
+    return namelist                
+
+def make_userids(names: list[list[str]]) -> list[list[str]]:
+    idlist: list[list[str]] = []
+    for name in names:
+        idnum: str = name[0].strip()
+        lastname: str = name[1].strip()
+        firstname: str = name[2].strip()
+        middlename: str = name[3].strip()
+        userid: str = lastname
+        if len(middlename) > 0:
+            userid = firstname[0] + middlename[0] + userid
+        else:
+            userid = firstname[0] + lastname
+        userid = clean(userid).lower()
+        idlist.append([idnum, userid, lastname, firstname, middlename])
+    return idlist
+
+
+def write_id_file(idlist: list[list[str]], fname: str) -> None:
+    with open(fname, 'w') as f:
+        # Write header line
+        f.write('idnumber,userid,lastname,firstname,middlename\n')
+        for id in idlist:
+            for i in range(4):
+                f.write(id[i] + ',')
+            f.write(id[4] + '\n')
+
 def main(args: list[str]) -> int:
     infilename: str = 'names.csv'
     outfilename: str = 'userids.csv'
 
-    with open(infilename,'r') as infile:
-        with open(outfilename, 'w') as outfile:
-            # Write header line to outfile
-            outfile.write('idnumber,userid,lastname,firstname,middlename\n')
-
-            for line in infile.readlines()[1:]:
-                # Split name into its components
-                # Pull off the last name
-                nameparts: list[str] = line.split(',')
-                lastname: str = nameparts[1]
-                givennames: list[str] = nameparts[2:]
-
-                # Clean up the last name
-                badchars: str = " '.,"  # No biscuit.  This string can be expanded as additional bad characters are encountered.
-                for c in badchars:
-                    lastname = lastname.replace(c, '')
-
-                id: str = ''
-                for name in givennames:
-                    id = id + name[0]
-                id = (id + lastname).lower() + '001'
-                
-                outfile.write('{0},{1},{2},{3},{4}\n'.format(nameparts[0], id, nameparts[1], nameparts[2], nameparts[3].strip()))
-
+    names: list[list[str]] = read_names(infilename)
+    idlist: list[list[str]] = make_userids(names)
+    write_id_file(idlist, outfilename)
 
     return 0 # Conventional return value for completing successfully
 
