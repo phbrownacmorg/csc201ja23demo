0451c851a3bb6f591a7ffd847f7968cda3b30313
Peter Brown <phbrown@acm.org>
2023-01-24 14:12:07 -0500

    Deeper into dictionaries

--- phonebook.py
+++ phonebook.py
@@ -5,28 +5,62 @@
 # just like an entry in a contacts list.  But for now, we'll stick to
 # the value being just the phone number.)
 
-def read_phonebook(fname: str) -> dict[str, str]:
-    phonebook: dict[str, str] = {}
+from datetime import date
+from typing import Any, cast
+
+def parse_user(values: list[str], fieldnames: list[str]) -> dict[str, Any]:
+    userdict: dict[str, Any] = {}
+    for i in range(len(fieldnames)):
+        field: str = fieldnames[i]
+        val: str = values[i].strip()
+        # Handle a couple of fields specially
+        # accesses is an int, default 0
+        if field == 'accesses':
+            if val == '':
+                userdict['accesses'] = 0
+            else:
+                userdict['accesses'] = int(val)
+        elif fieldnames[i] == 'lastchanged':
+            userdict[field] = date.fromisoformat(val)
+        # Everything else is just a string
+        else:
+            userdict[field] = val
+
+    return userdict
+
+def read_phonebook(fname: str) -> dict[str, Any]:
+    phonebook: dict[str, Any] = {}
     with open(fname, 'r') as f:
-        for line in f.readlines()[1:]:
-            parts: list[str] = line.split(',')
-            name: str = parts[0].strip()
-            number: str = parts[1].strip()
-            phonebook[name] = number
+        lines: list[str] = f.readlines()
+        # Read the fields
+        fields: list[str] = lines[0].strip().split(',')
+        # read the entries
+        for line in lines[1:]:
+            parts = line.strip().split(',')
+            userdict: dict[str, Any] = parse_user(parts, fields)
+            phonebook[userdict['name']] = userdict
 
     return phonebook
 
+def print_entry(phonebook: dict[str, Any], name: str) -> None:
+    try:
+        entry: dict[str, Any] = phonebook[name]
+        # Increase the access count
+        entry['accesses'] = entry['accesses'] + 1
+    except KeyError:
+            print('The phonebook has no entry for ' + name)
+    else:
+        print('Entry for {0}:'.format(name))
+        for field in entry.keys():
+            print('\t{0:11}\t{1}'.format(field + ':', entry.get(field, '')))
+
 def main(args: list[str]) -> int:
-    phonebook: dict[str, str] = read_phonebook('phonebook.csv')
+    phonebook: dict[str, dict[str, Any]] = read_phonebook('phonebook.csv')
     #print(phonebook)
 
     name: str = input('Please enter a name to look up, or hit Enter to quit: ')
     while name != '':
-        try:
-            number: str = phonebook[name]
-            print('The number for {0} is {1}.'.format(name, number))
-        except KeyError:
-            print('The phonebook has no number for ' + name)
+        print_entry(phonebook, name)
         name = input('Please enter a name to look up, or hit Enter to quit: ')
 
     return 0 # Conventional return value for completing successfully
